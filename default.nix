ca6331d49829e0cce8452dd21d912a8bcd37aaf3
Moritz Kiefer <moritz.kiefer@purelyfunctional.org>
2018-07-12 17:47:41 +0200

    Update nix config

--- default.nix
+++ default.nix
@@ -1,5 +1,5 @@
-{ rev ? "95a8cb3ade1ad0e2649f0f3128e90c53964af5e1",
-  outputSha256 ? "0jxn25l8d65h6qnmx9f4dsi2fscqyxc0gvhnkj1bc7kicn9rr5hj",
+{ rev ? "1539167d215841a966b8395c1025d66812d63d31",
+  outputSha256 ? "08371nrlz19ah3q2yqcr3z2268n46ilrn7d399izg81bjpw744nk"
 }:
 let
   nixpkgs = builtins.fetchTarball {
@@ -7,19 +7,25 @@ let
     sha256 = outputSha256;
   };
   pkgs = import nixpkgs {};
+  robinhood-src = pkgs.lib.cleanSourceWith {
+    src = pkgs.lib.cleanSource ./.;
+    filter = path: type:
+      let base = baseNameOf path;
+      in !(pkgs.lib.hasPrefix ".ghc.environment." base) &&
+         !(pkgs.lib.hasSuffix ".nix" base);
+  };
   haskellPackages = pkgs.haskell.packages.ghc843.override(old: {
     all-cabal-hashes = builtins.fetchurl {
       url = "https://github.com/commercialhaskell/all-cabal-hashes/archive/e8599faa9bb6158597fa849fb9f466ee385fb0d9.tar.gz";
       sha256 = "19h087c16hh8kksm73cifzizzg46dk2ww755xz2i95z2nks82div";
     };
     overrides = self: super: {
-      robinhood = pkgs.haskell.lib.overrideCabal (super.callCabal2nix "robinhood" ./. {
+      robinhood = pkgs.haskell.lib.overrideCabal (super.callCabal2nix "robinhood" robinhood-src {
       }) (old: {
         doBenchmark = true;
         configureFlags = "--enable-benchmarks";
       });
-      contiguous = super.callHackage "contiguous" "0.2.0.0" {};
-      primitive = super.callHackage "primitive" "0.6.4.0" {};
+      primitive = super.primitive_0_6_4_0;
       vector-hashtables = super.callCabal2nix "vector-hashtables" (pkgs.fetchFromGitHub {
         owner = "klapaucius";
         repo = "vector-hashtables";
